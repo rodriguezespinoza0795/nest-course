02f4ccfca51801e7109770371b804efdd25cb56d
davidre0795 <davidre0795@gmail.com>
2024-06-24 15:40:52 -0600

    02-objects

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { name, age } from './bases/01-types'
+import { bulbasaur } from './bases/02-objects';
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    Mi nombre es ${name} tengo ${age} años 
+    Mi nombre es ${name} tengo ${age} años
+    <h1>Hello ${ bulbasaur.name } ${ bulbasaur.id }!</h1>
   </div>
 `
